0ac373a352876683fd288fc23428f2cacb087b1a
NJJPublishing <132226880+NJJPublishing@users.noreply.github.com>
2023-05-26 13:41:44 -0400

    Footer v3

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,18 +6,19 @@ const Footer = () => {
   const currentYear = getCurrentYear();
   const twitterLink =
     "https://twitter.com/ApesTogetherDoc";
-  const sourceCodeLink = "https://www.apestogetherstrongdoc.com/";
+  const sourceCodeLink = "https://www.apestogetherstrongdoc.com";
 
   return (
     <div className="py-4 px-8 w-full flex flex-col md:flex-row items-center justify-between space-y-1 md:space-y-0 mt-auto text-center">
       <p className="text-white/40">
         {currentYear},{" "}
-        <FooterLink href="https://www.apestogetherstrongdoc.com/">Apes Together Strong</FooterLink> | 
-        A Mulligan Brothers Documentary
+        <FooterLink href="https://twitter.com/ApesTogetherDoc">Apes Together Strong</FooterLink> — 
+        A Mulligan Brothers Documentary.
       </p>
       <div className="space-x-4">
         <FooterLink href={sourceCodeLink}>
-          <p className="hidden md:inline-block">Visit Our Website/p>
+          <p className="hidden md:inline-block">Visit Our Website</p>
+          <GithubIcon className="w-5 h-5" />
         </FooterLink>
         <FooterLink href={twitterLink}>
           <p className="hidden md:inline-block"> Follow Us On Twitter</p>
