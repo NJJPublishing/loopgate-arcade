6e530c7c43be9fd77c5981f99e82cc4dfec5d8e9
NJJ Publishing <132226880+NJJPublishing@users.noreply.github.com>
2023-05-31 15:10:44 -0400

    Amend Footer Links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,6 +7,9 @@ const Footer = () => {
   const twitterLink =
     "https://twitter.com/ApesTogetherDoc";
   const sourceCodeLink = "https://www.apestogetherstrongdoc.com/";
+  const shopLink = "https://www.apestogetherstrongdoc.com/store";
+  const privacyPolicyLink = "https://www.apestogetherstrongdoc.com/privacy-policy";
+  const termsLink = "https://www.apestogetherstrongdoc.com/terms-and-conditions";
 
   return (
     <div className="py-4 px-8 w-full flex flex-col md:flex-row items-center justify-between space-y-1 md:space-y-0 mt-auto text-center">
@@ -15,11 +18,17 @@ const Footer = () => {
         <FooterLink href="https://www.apestogetherstrongdoc.com">Apes Together Strong</FooterLink> | A Mulligan Brothers Documentary
       </p>
       <div className="space-x-4">
-        <FooterLink href={sourceCodeLink}>
-          <p className="hidden md:inline-block">Visit Our Website</p>
+        <FooterLink href={shopLink}>
+          <p className="hidden md:inline-block">Shop</p>
         </FooterLink>
         <FooterLink href={twitterLink}>
-          <p className="hidden md:inline-block"> Follow Us On Twitter</p>
+          <p className="hidden md:inline-block">Twitter</p>
+        </FooterLink>
+        <FooterLink href={privacyPolicyLink}>
+          <p className="hidden md:inline-block"> Privacy Policy</p>
+        </FooterLink>
+        <FooterLink href={termsLink}>
+          <p className="hidden md:inline-block"> Terms & Conditions</p>
         </FooterLink>
       </div>
     </div>
