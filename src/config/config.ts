73d3c777e2634a7a4a4b6564068b73ff2d1ac933
NJJ Publishing <132226880+NJJPublishing@users.noreply.github.com>
2023-06-10 15:18:39 -0400

    Update NFT Keys

--- src/config/config.ts
+++ src/config/config.ts
@@ -23,31 +23,31 @@ const unlockables: Unlockable[] = [
   {
     cid: "bafybeif42jlthaiy7m7frxp6rsyfeqvxlmr2cpio4oiqfsjthurc6cxloa", // ATS Video
     nftId: [
-      "0xbdce4c9a5a90d253920dacb1630be43a4cc22939023f0d2476e4ead6996aef79",
+      "0xde2839b3dea14689613bb52ea320e660f1071b582a2481c6bc577fce7ad671b0",
     ], // ATS Video
   },
   {
     cid: "bafybeibbfa7zxtjxy56kw345n3r6wpdwrln3dn7po623lwcmzf7l576twa", // Deleted Scene with u/Buttfarm69 Video
     nftId: [
-      "0xbdce4c9a5a90d253920dacb1630be43a4cc22939023f0d2476e4ead6996aef79",
+      "0xde2839b3dea14689613bb52ea320e660f1071b582a2481c6bc577fce7ad671b0",
     ], // Deleted Scene with u/Buttfarm69 Video
   },
   {
     cid: "bafybeifp4kct5qzgprjmt7jyi5jf5hdksp7vezpet6qkzpc5www56w2tnm", // ATS Video Game (iFrame NJJ Full Version)
     nftId: [
-      "0xbdce4c9a5a90d253920dacb1630be43a4cc22939023f0d2476e4ead6996aef79",
+      "0xde2839b3dea14689613bb52ea320e660f1071b582a2481c6bc577fce7ad671b0",
     ], // ATS Video Game (iFrame NJJ Full Version)
   },
   {
     cid: "bafybeifxxpoeoxzv4dx5jdsjc6l6f3n2qd4ibpeiwlwj4tjcngcbat7heu", // VIP Placeholder 01
     nftId: [
-      "0xbdce4c9a5a90d253920dacb1630be43a4cc22939023f0d2476e4ead6996aef79",
+      "0xde2839b3dea14689613bb52ea320e660f1071b582a2481c6bc577fce7ad671b0",
     ], // VIP Placeholder 01
   },
   {
     cid: "bafybeic2g73iaqlqjsgtmadlsiyqah23qnjsjlevs4byxyqo7zkhvj4duq", // VIP Placeholder 02
     nftId: [
-      "0xbdce4c9a5a90d253920dacb1630be43a4cc22939023f0d2476e4ead6996aef79",
+      "0xde2839b3dea14689613bb52ea320e660f1071b582a2481c6bc577fce7ad671b0",
     ], // VIP Placeholder 02
   },
 ];
