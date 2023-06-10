1d98ed350329d13f084ff3949db9415717da1ca4
NJJ Publishing <132226880+NJJPublishing@users.noreply.github.com>
2023-06-10 14:22:07 -0400

    Add Game

--- src/config/config.ts
+++ src/config/config.ts
@@ -33,6 +33,12 @@ const unlockables: Unlockable[] = [
     ], // Deleted Scene with u/Buttfarm69 Video
   },
   {
+    cid: "bafybeie3ux7aqw3qxiwb4mf3wht25t6whrplavaqb4yrl5draiki427b6e", // ATS Video Game (Submarined Version)
+    nftId: [
+      "0xbdce4c9a5a90d253920dacb1630be43a4cc22939023f0d2476e4ead6996aef79",
+    ], // ATS Video Game (Submarined Version)
+  },
+  {
     cid: "bafybeiafbmqzo4x23nqnnftaism55wv2lbxt7fomvvoi6bqkvfk2ocyziq", // VIP Placeholder 01
     nftId: [
       "0xbdce4c9a5a90d253920dacb1630be43a4cc22939023f0d2476e4ead6996aef79",
